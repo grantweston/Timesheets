17935899b8dc4dd7ee5d29e6dbf1bc24c0aa0bc0
Marcus <mvj.palsson@gmail.com>
2025-02-24 16:36:52 +1100

    sign out from main page works

--- apps/frontend/.next/server/webpack-runtime.js
+++ apps/frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("188264ccbd4821c4")
+/******/ 		__webpack_require__.h = () => ("79835fcdda137f50")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,11 +163,6 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
